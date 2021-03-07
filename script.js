05cab15d78903b29547ae2d32d094bab3b008467
KevinScheffler <schefk2122@gmail.com>
2021-03-07 10:13:24 -0500

    Day 2

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ function factorialize(num) {
 }
 
 // console.log(factorialize(5));
+
+
+// Find the Longest Word in a String
+
+function findLongestWordLength(str) {
+    let strSplit = str.split(' ');
+    let maxLength = 0;
+    for (let i = 0; i < strSplit.length; i++) {
+        if (strSplit[i].length > maxLength) {
+            maxLength = strSplit[i].length;
+        }
+    }
+    return maxLength;
+}
+
+console.log(findLongestWordLength
+("The quick brown fox jumped over the lazy dog"));
