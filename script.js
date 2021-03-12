4d2831e2b8fd685f7103a7aabf81773f7107583b
KevinScheffler <schefk2122@gmail.com>
2021-03-12 08:15:25 -0500

    Day 7

--- script.js
+++ script.js
@@ -87,3 +87,20 @@ function truncateString(str, num) {
 
 // console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
 // console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length))
+
+
+
+
+// FINDERS KEEPERS
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i];
+        if(func(num)) {
+            return num;
+        }
+    }
+    return undefined;
+}
+
+console.log(findElement([1, 2, 3, 4], num => num % 2 === 0));
