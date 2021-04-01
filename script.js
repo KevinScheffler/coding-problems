87b0f6698e5681ff2511b0925040b7cbc0ad0c44
KevinScheffler <schefk2122@gmail.com>
2021-04-01 17:35:24 -0400

    Cat Years Dog Years

--- script.js
+++ script.js
@@ -203,4 +203,28 @@ function mouthSize(animal) {
     return animal === 'alligator' ? 'small' : 'wide';
 }
 
-console.log(mouthSize('alligator'));
+// console.log(mouthSize('alligator'));
+
+// CAT YEARS DOG YEARS
+const humanYearsCatYearsDogYears = (humanYears) => {
+    let catYears = 0;
+    let dogYears = 0;
+    
+    for (let i = 1; i <= humanYears; i++) {
+      if (i === 1) {
+        catYears += 15;
+        dogYears += 15;
+      }
+      else if (i === 2) {
+        catYears += 9;
+        dogYears += 9;
+      }
+      else {
+        catYears += 4;
+        dogYears += 5;
+      }
+    }
+    return [humanYears,catYears,dogYears];
+  }
+
+  console.log(humanYearsCatYearsDogYears(2));
