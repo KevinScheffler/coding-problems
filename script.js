ebd3a8f55a898f9df0ef71ee2b0cf5602e50a66d
KevinScheffler <schefk2122@gmail.com>
2021-04-06 16:48:58 -0400

    frankensplice

--- script.js
+++ script.js
@@ -250,4 +250,15 @@ function generateRange(min, max, step) {
     return arr;
 }
 
-console.log(generateRange(2, 10, 2));
+// console.log(generateRange(2, 10, 2));
+
+function frankenSplice(arr1, arr2, n) {
+    let localArray = arr2.slice();
+    for (let i = 0; i < arr1.length; i++) {
+        localArray.splice(n, 0, arr1[i]);
+        n++;
+    }
+    return localArray;
+}
+
+console.log(frankenSplice([1, 2, 3], [4, 5, 6], 1));
