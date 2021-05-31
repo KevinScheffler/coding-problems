ec41029adc1b1b1830df537bc34b7f7340e225c2
KevinScheffler <schefk2122@gmail.com>
2021-05-31 14:00:57 -0400

    Opposites Attract

--- script.js
+++ script.js
@@ -418,5 +418,24 @@ function sumStr(a,b) {
     return String(Number(a)+Number(b));
 }
 
-console.log(sumStr(4,"5"));
-console.log(sumStr("34","5"));
+// console.log(sumStr(4,"5"));
+// console.log(sumStr("34","5"));
+
+
+
+
+// Opposites Attract
+function lovefunc(flower1, flower2) {
+    if (flower1 % 2 === 0 && flower2 % 2 ===  1) {
+        return true;
+    } else if (flower1 % 2 === 1 && flower2 % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(lovefunc(1,4));
+console.log(lovefunc(2,2));
+console.log(lovefunc(0,1));
+console.log(lovefunc(0,0));
